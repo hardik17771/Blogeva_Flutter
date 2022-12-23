de5bef9fb45f62a4ca1e7dc2586f7be73115d40b
Hardik Sachdeva <hardiksachdeva52@gmail.com>
2022-12-23 23:48:33 +0530

    error fix

--- lib/signup_page.dart
+++ lib/signup_page.dart
@@ -43,17 +43,93 @@ class _SignUpPageState extends State<SignUpPage> {
     );
 
   }
-  Widget usernameKaText(String label)
-  {
-    return Column(
-      children: [
-        Text(label),
-        TextFormField(
-          decoration: InputDecoration(
+  Widget usernameTextField() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10.0),
+      child: Column(
+        children: [
+          Text("Username"),
+          TextFormField(
+            controller: _usernameController,
+            decoration: InputDecoration(
+              errorText: validate ? null : errorText,
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.black,
+                  width: 2,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
 
-          ),
-        )
-      ],
+  Widget emailTextField() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10.0),
+      child: Column(
+        children: [
+          Text("Email"),
+          TextFormField(
+            controller: _emailController,
+            validator: (value) {
+              if (value.isEmpty) return "Email can't be empty";
+              if (!value.contains("@")) return "Email is Invalid";
+              return null;
+            },
+            decoration: InputDecoration(
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.black,
+                  width: 2,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget passwordTextField() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10.0),
+      child: Column(
+        children: [
+          Text("Password"),
+          TextFormField(
+            controller: _passwordController,
+            validator: (value) {
+              if (value.isEmpty) return "Password can't be empty";
+              if (value.length < 8) return "Password lenght must have >=8";
+              return null;
+            },
+            obscureText: vis,
+            decoration: InputDecoration(
+              suffixIcon: IconButton(
+                icon: Icon(vis ? Icons.visibility_off : Icons.visibility),
+                onPressed: () {
+                  setState(() {
+                    vis = !vis;
+                  });
+                },
+              ),
+              helperText: "Password length should have >=8",
+              helperStyle: TextStyle(
+                fontSize: 14,
+              ),
+              focusedBorder: UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: Colors.black,
+                  width: 2,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 }
